f6415b292e2dcb57a682382829c7c5215ef153f0
tallan groberg <tallan.taven@gmail.com>
2019-09-04 22:19:27 -0600

    init

---
+++ businessTime.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+const showMark = () => {
+  const mark = document.getElementById("markus").innerHTML =  " <h1>You're going to rome </h1> <img src='images/markus.jpeg' alt='ja' /> <br />  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ut numquam dolore, fugit sequi voluptatum.</p>"
+  console.log(mark)
+}
+
+const showNeb = () => {
+  const neb = document.getElementById("neb").innerHTML =  " <h1>You're going to the nebula </h1> <img src='images/neb.jpeg' alt='ja' /> <br />  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ut numquam dolore, fugit sequi voluptatum.</p>"
+  console.log(neb)
+}
+
+const showWood = () => {
+  const wood = document.getElementById("wood").innerHTML =  " <h1>You're going to woodstock!!! </h1> <img src='images/woods.jpeg' alt='ja' /> <br />  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ut numquam dolore, fugit sequi voluptatum.</p>"
+  console.log(wood)
+}
+
